2324b3be9aa05f45ce97048edff85c9fcf0d6a53
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-10-17 12:43:41 +1100

    :zap: updating TEI for S155+

--- mk-reports.sh
+++ mk-reports.sh
@@ -1,2 +1,4 @@
 teirubrics by-date S155.xml S73.xml --output reports/dates.html
 teirubrics by-verse S155.xml S73.xml --verse-list ArabGr1-Verses.txt --output reports/verses.html
+teirubrics by-date S155.xml --output reports/dates-S155.html
+teirubrics by-verse S155.xml  --verse-list ArabGr1-Verses.txt --output reports/verses-S155.html
